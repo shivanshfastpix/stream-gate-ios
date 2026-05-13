5b75c8a75542f99c263d1dd1baf2bcf8cc5fbad0
shivansh chouhan <shivansh@fastpix.com>
2026-05-13 12:03:29 +0530

    Initial Commit

---
+++ StreamGate/StreamGateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamGateApp.swift
+//  StreamGate
+//
+//  Created by Shivansh Chouhan on 13/05/26.
+//
+
+import SwiftUI
+
+@main
+struct StreamGateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
